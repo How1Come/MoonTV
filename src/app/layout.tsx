8d41262f1341728a1ae82fb88d83b6312febdd2b
How1Come <118341976+How1Come@users.noreply.github.com>
2026-02-19 15:12:27 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,7 +97,7 @@ export default async function RootLayout({
         />
         {/* 将配置序列化后直接写入脚本，浏览器端可通过 window.RUNTIME_CONFIG 获取 */}
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <meta name="referrer" content="no-referrer">
+        <meta name="referrer" content="no-referrer"/>
         <script
           dangerouslySetInnerHTML={{
             __html: `window.RUNTIME_CONFIG = ${JSON.stringify(runtimeConfig)};`,
